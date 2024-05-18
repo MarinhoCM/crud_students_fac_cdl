4b488d7515315c86ac2c074f4f64c36783d53e20
MarinhoCM <marinhoc384@gmail.com>
2024-05-18 19:53:23 -0300

    :tada: init: inicializando projeto cake

---
+++ src/Model/Table/TeachersTable.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class TeachersTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+    }
+}
